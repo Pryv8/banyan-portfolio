fdb6b55de318d532ba3a2192ef0900629e43364f
Pryv8 <Pr1v8lane@gmail.com>
2023-03-27 15:23:53 -0700

    the whole code for my portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/Services/Services'
 import Portfolio from './components/Portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Banner from './components/about/Banner'
 const App = () => {
   return (
     <>
         <Header />
         <NavBar />
+        <Banner />
         <About />
         <Experience />
         <Services />
         <Portfolio />
-        {/* <Testimonials /> */}
         <Contact />
         <Footer />
     </>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 import './contact.css'
 import {SiGmail} from 'react-icons/si'
 import{RiMessengerLine} from 'react-icons/ri'
 import{BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6s87wbs', 'template_obi7l9q', form.current, 'j4Dnrh_EguNguehIv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
   return (
     <section id='contact'>
     <h5>Get In Touch</h5>
@@ -17,25 +33,25 @@ const Contact = () => {
 <SiGmail className="contact_option-icon"/>
 <h4>Email</h4>
 <h5>Pr1v8lane@gmail.com</h5>
-<a href="mailto:Pr1v8lane@gmail.com" target="_blank">Send a message</a>
+<a href="mailto:Pr1v8lane@gmail.com" target="_blank" rel='noreferrer'>Send a message</a>
 </article>
 
 <article className="contact_option">
 <RiMessengerLine className="contact_option-icon"/>
 <h4>Messenger</h4>
 <h5>Dev.Gilmore</h5>
-<a href="https://m.me/profile.php?id=100088874928268" target="_blank">Send a message</a>
+<a href="https://m.me/100088874928268" target="_blank" rel='noreferrer'>Send a message</a>
 </article>
 
 <article className="contact_option">
 <BsWhatsapp className="contact_option-icon"/>
 <h4>WhatsApp</h4>
 <h5>+123456789</h5>
-<a href="https://api.whatsapp.com/send?phone+3526155552" target="_blank">Send a message</a>
+<a href="https://api.whatsapp.com/send?phone+3526155552" target="_blank" rel='noreferrer'>Send a message</a>
 </article>
     </div>
     {/* END OF CONTACT OPTIONS */}
-    <form action="">
+    <form ref={form} onSubmit={sendEmail}>
 <input type="text" name="name" placeholder='Your Full Name' required />
 <input type="email" name="email" placeholder='Your Email' required/>
 <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
